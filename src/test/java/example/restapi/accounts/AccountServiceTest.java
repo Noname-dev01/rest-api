9a35c993979400d33ecbd6155011e50562f05dbe
noname_dev <noname_dev@naver.com>
2023-10-13 18:31:43 +0900

    예외 테스트

--- src/test/java/example/restapi/accounts/AccountServiceTest.java
+++ src/test/java/example/restapi/accounts/AccountServiceTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -40,5 +41,12 @@ class AccountServiceTest {
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
         assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
+
+    @Test
+    public void findByUsernameFail(){
+        String username = "random@email.com";
+        assertThatThrownBy(() -> accountService.loadUserByUsername(username))
+                .isInstanceOf(UsernameNotFoundException.class);
     }
 }
