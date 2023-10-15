aeae1a90c50d45c80d033f774d9d8f2fbc3b5960
noname_dev <noname_dev@naver.com>
2023-10-15 18:09:34 +0900

    Bad Request 응답

--- src/test/java/example/restapi/events/EventControllerTests.java
+++ src/test/java/example/restapi/events/EventControllerTests.java
@@ -119,7 +119,11 @@ public class EventControllerTests {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(eventDto))
                 )
-                .andExpect(status().isBadRequest());
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$[0].objectName").exists())
+                .andExpect(jsonPath("$[0].defaultMessage").exists())
+                .andExpect(jsonPath("$[0].code").exists());
     }
 
 }
