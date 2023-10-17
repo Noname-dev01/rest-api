054a5addbb00143aadcb9dcab0fc16ae791a26e4
noname_dev <noname_dev@naver.com>
2023-10-17 17:50:46 +0900

    비즈니스 로직 적용

--- src/test/java/example/restapi/events/EventTest.java
+++ src/test/java/example/restapi/events/EventTest.java
@@ -33,4 +33,55 @@ public class EventTest {
         assertThat(event.getDescription()).isEqualTo(description);
     }
 
+    @Test
+    public void testFree() {
+        Event event = Event.builder()
+                .basePrice(0)
+                .maxPrice(0)
+                .build();
+
+        event.update();
+
+        assertThat(event.isFree()).isTrue();
+
+        event = Event.builder()
+                .basePrice(100)
+                .maxPrice(0)
+                .build();
+
+        event.update();
+
+        assertThat(event.isFree()).isFalse();
+
+        event = Event.builder()
+                .basePrice(0)
+                .maxPrice(100)
+                .build();
+
+        event.update();
+
+        assertThat(event.isFree()).isFalse();
+
+
+    }
+
+    @Test
+    public void testOffline(){
+        Event event = Event.builder()
+                .location("강남역 네이버")
+                .build();
+
+        event.update();
+
+        assertThat(event.isOffline()).isTrue();
+
+        event = Event.builder()
+                .build();
+
+        event.update();
+
+        assertThat(event.isOffline()).isFalse();
+
+    }
+
 }
