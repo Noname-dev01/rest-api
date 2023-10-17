39aa0f306fded33c76399e517ce23835aa119308
noname_dev <noname_dev@naver.com>
2023-10-17 22:59:40 +0900

    스프링 HATEOAS 적용

---
+++ src/main/java/example/restapi/events/EventResource.java
@@ -0,0 +1,19 @@
+package example.restapi.events;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.util.Arrays;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+public class EventResource extends EntityModel<Event> {
+
+
+    public EventResource(Event event, Link... links){
+        super(event, Arrays.asList(links));
+        add(linkTo(EventController.class).slash(event.getId()).withSelfRel());
+    }
+
+}
